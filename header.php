8ab761595b69ef9baffe26ffc76b59f381951abe
Chad A. Carino <chadc@tekserve.com>
2016-02-22 17:17:12 -0500

    menu stuff

--- header.php
+++ header.php
@@ -49,12 +49,12 @@
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<div id="logo" class="h1" itemscope itemtype="http://schema.org/Organization">
+					<span id="logo" class="h1" itemscope itemtype="http://schema.org/Organization">
 						<a href="/" rel="nofollow">
 						<img class="base_img" src="/wp-content/themes/sundial_bones/library/images/menu_logo-base.png" alt="Madam C.J. Walker." />
 						<img class="sseup_img" src="/wp-content/themes/sundial_bones/library/images/menu_logo-768.png" alt="Madam C.J. Walker." />
 						</a>
-					</div>
+					</span>
 
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
@@ -64,9 +64,9 @@
 					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
 						<?php wp_nav_menu(array(
     					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+    					         'container_class' => 'menu',                 // class of container (should you choose to use it)
     					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
+    					         'menu_class' => 'nav top-nav',               // adding custom nav class
     					         'theme_location' => 'main-nav',                 // where it's located in the theme
     					         'before' => '',                                 // before the menu
         			               'after' => '',                                  // after the menu
