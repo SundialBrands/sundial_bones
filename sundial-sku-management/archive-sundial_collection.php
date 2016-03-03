bffbff886aa2e13f4efac4897f6a7663332fa618
Chad A. Carino <ccarino@sundialbrands.com>
2016-03-03 14:36:56 -0500

    what was called for, clearly, was a more complex structure

--- sundial-sku-management/archive-sundial_collection.php
+++ sundial-sku-management/archive-sundial_collection.php
@@ -43,116 +43,123 @@ $product_navcats = array(
 				
 				?>
 			</header><!-- .page-header -->
-			<div class="previous_sundial_static_post">
-				<i class="fa fa-chevron-left"></i>
-			</div>
-			<div class="next_sundial_static_post">
-				<i class="fa fa-chevron-right"></i>
-			</div>
-			<?php echo '<div class="sundial_prev_static_post"></div>'; ?>
-			<?php
-			// Start the Loop.
-			$i=0;
-			while ( have_posts() ) : the_post();
+			<div class="sundial_static_post_containers">
+				<div class="previous_sundial_static_post">
+					<i class="fa fa-chevron-left"></i>
+				</div>
+				<div class="next_sundial_static_post">
+					<i class="fa fa-chevron-right"></i>
+				</div>
+				<div class="sundial_prev_static_post">
+				</div>
+				<?php
+				// Start the Loop.
+				$i=0;
+				while ( have_posts() ) : the_post();
 
-				$collection = get_collection_meta( get_the_ID(), $product_navcats );
-				$nsarray = wp_get_post_terms( get_the_ID(), 'need_states' );
-				$need_state = '';
-				foreach( $nsarray as $state ) {
-					if( strpos( strtolower( $state->name ), 'for ' ) === 0 ) {
-						$need_state = $state->name;
+					$collection = get_collection_meta( get_the_ID(), $product_navcats );
+					$nsarray = wp_get_post_terms( get_the_ID(), 'need_states' );
+					$need_state = '';
+					foreach( $nsarray as $state ) {
+						if( strpos( strtolower( $state->name ), 'for ' ) === 0 ) {
+							$need_state = $state->name;
+						}
 					}
-				}
 				
-				$color = strtolower( $collection['color1'] );
-				switch( $color ) {
+					$color = strtolower( $collection['color1'] );
+					switch( $color ) {
 				
-					case '#a7a9ac':
-						$color = 'gray';
-						break;
-					case '#af95c6':
-						$color = 'lilac';
-						break;
-					case '#ce7e9c':
-						$color = 'pink';
-						break;
-					case '#63b0bb':
-						$color = 'teal';
-						break;
-					case '#ecba4b':
-						$color = 'yellow';
-						break;
-					default:
-						$color = '';
+						case '#a7a9ac':
+							$color = 'gray';
+							break;
+						case '#af95c6':
+							$color = 'lilac';
+							break;
+						case '#ce7e9c':
+							$color = 'pink';
+							break;
+						case '#63b0bb':
+							$color = 'teal';
+							break;
+						case '#ecba4b':
+							$color = 'yellow';
+							break;
+						default:
+							$color = '';
 				
-				}
+					}
 				
-				?>
+					?>
 				
-				<?php if( $i == 0 ) { echo '<div class="sundial_active_static_post">'; } ?>
-				<?php if( $i == 1 ) { echo '<div class="sundial_next_static_post">'; } ?>
-				<?php if( $i > 1 ) { echo '<div class="sundial_numbered_static_post right-side number' . $i . '">'; } ?>
+					<?php if( $i == 0 ) { echo '<div class="sundial_active_static_post">'; } ?>
+					<?php if( $i == 1 ) { echo '<div class="sundial_next_static_post">'; } ?>
+					<?php if( $i > 1 ) { echo '<div class="sundial_numbered_static_post right-side number' . $i . '">'; } ?>
 				
-					<article id="post-<?php the_ID(); ?>" <?php post_class( $color ); ?> >
-						<div class="sundial_static_post_container">
-							<div class="sundial_static_post wrap cf">
+						<article id="post-<?php the_ID(); ?>" <?php post_class( $color ); ?> >
+							<div class="sundial_static_post_container">
+								<div class="sundial_static_post wrap cf">
 								
-								<div class="sundial_static_post-benefit">
-									<h2><?php echo $collection['tagline']; ?></h2>
-								</div>
+									<div class="sundial_static_post-benefit">
+										<h2><?php echo $collection['tagline']; ?></h2>
+									</div>
 						
-								<div class="sundial_static_post-title" style="color:<?php echo $collection['color2'] ?>;">
-									<h1><?php the_title(); ?></h1>
-								</div>
+									<div class="sundial_static_post-title" style="color:<?php echo $collection['color2'] ?>;">
+										<h1><?php the_title(); ?></h1>
+									</div>
 						
-								<div class="sundial_static_post-image">
-									<?php the_post_thumbnail( 'large', array( 'class' => 'aligncenter collection-image' ) ); ?>
-									<img class="sundial_static_post-burst" src="<?php echo get_template_directory_uri() . '/library/images/burst.svg' ?>" />
-								</div>
+									<div class="sundial_static_post-image">
+										<?php the_post_thumbnail( 'large', array( 'class' => 'aligncenter collection-image' ) ); ?>
+										<img class="sundial_static_post-burst" src="<?php echo get_template_directory_uri() . '/library/images/burst.svg' ?>" />
+									</div>
 							
 					
-								<footer class="entry-footer">
-								<?php
-									edit_post_link(
-										sprintf(
-											/* translators: %s: Name of current post */
-											'<i class="fa fa-pencil-square-o fa-2x"></i>Edit<span class="screen-reader-text"> "%s"</span>',
-											get_the_title()
-										),
-										'<span class="sundial-button edit-link">',
-										'</span>'
-									);
-								?>
-								</footer><!-- .entry-footer -->
-							</div>
-							<div class="sundial_static_post_block" style="background:<?php echo $collection['color1'] ?>;">
+									<footer class="entry-footer">
+									<?php
+										edit_post_link(
+											sprintf(
+												/* translators: %s: Name of current post */
+												'<i class="fa fa-pencil-square-o fa-2x"></i>Edit<span class="screen-reader-text"> "%s"</span>',
+												get_the_title()
+											),
+											'<span class="sundial-button edit-link">',
+											'</span>'
+										);
+									?>
+									</footer><!-- .entry-footer -->
+								</div>
+								<div class="sundial_static_post_block" style="background:<?php echo $collection['color1'] ?>;">
 						
-								<div class="sundial_static_post-needstate">
-									<h2><?php echo $need_state; ?></h2>
+									<div class="sundial_static_post-needstate">
+										<h2><?php echo $need_state; ?></h2>
+									</div>
+						
 								</div>
 						
-							</div>
-						
-							<div class="product-information">
-								<div class="interior"></div>
-								<div class="description">
-									<?php the_content(); ?>
+								<div class="product-information">
+									<div class="interior"></div>
+									<div class="description">
+										<?php the_content(); ?>
+									</div>
 								</div>
-							</div>
 							
-						</div>
+							</div>
 
-					</article>
+						</article>
 				
-				</div>		
-			<?php
+					</div>		
+				<?php
 
-				$i++;
-			// End the loop.
-			endwhile;
+					$i++;
+				// End the loop.
+				endwhile; ?>
+			
+			</div>
+			
+		<?php
 
-
-		// If no content, include the "No posts found" template.
+		// If no content, return nothing.
+		
+		//nothing.
 		
 
 		endif;
