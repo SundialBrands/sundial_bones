decd3c2ea38bc47572f957710d428962922668d3
Chad A. Carino <ccarino@sundialbrands.com>
2016-04-11 08:42:51 -0400

    live branch UTD

--- functions.php
+++ functions.php
@@ -426,4 +426,15 @@ function get_head( $name = null ) {
 			load_template( ABSPATH . WPINC . '/theme-compat/header.php');
 }
 
+//Allow Contributors to Add Media
+if ( current_user_can('contributor') && !current_user_can('upload_files') )
+add_action('admin_init', 'allow_contributor_uploads');
+  
+function allow_contributor_uploads() {
+     $contributor = get_role('contributor');
+     $contributor->add_cap('upload_files');
+}
+
+
+
 /* DON'T DELETE THIS CLOSING TAG */ ?>
